0ffe1395e8f4445216510d4f2ccb6cc615134964
xchedeiklo <xchedeiklo@gmail.com>
2019-07-10 10:30:29 -0700

    Update screenshare.py

--- screenshare/screenshare.py
+++ screenshare/screenshare.py
@@ -15,21 +15,10 @@ class Screenshare(commands.Cog):
             Creates a screenshare link for voicechannel
             
         """
-
-        user = ctx.message.author
         vc = ctx.message.author.voice.channel
         sid = str(ctx.guild.id)
         vcid = str(ctx.message.author.voice.channel.id)
-        #await ctx.send(voicechannelid)
+	if not vc:
+		await ctx.send("PLease join a voice channel first")
+	else:
         await ctx.send(text_string.format(voicechannel = vc) + link_string.format(serverid = sid, voicechannelid = vcid))
-	
-
-
-#        if victim.id == self.bot.user.id:
-  #          await ctx.send("I refuse to kill myself!")
-   #         
-    #    elif victim.id == user.id:            
-     #       await ctx.send(choice(kill_list).format(victim = user.display_name, killer = self.bot.user.display_name))
-#
- #       else:
-#            await ctx.send(choice(kill_list).format(victim = victim.display_name, killer = user.display_name))
