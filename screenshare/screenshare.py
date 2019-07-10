c8f3e4f90f78b282845067ebfe9517b7a5790aa3
xchedeiklo <xchedeiklo@gmail.com>
2019-07-10 10:36:54 -0700

    Update screenshare.py

--- screenshare/screenshare.py
+++ screenshare/screenshare.py
@@ -15,11 +15,14 @@ class Screenshare(commands.Cog):
             Creates a screenshare link for voicechannel
             
         """
+        
+        sid = str(ctx.guild.id)
+        # get guild(server id)
         try:
                 vc = ctx.message.author.voice.channel
+                vcid = str(ctx.message.author.voice.channel.id)
         except:
                 await ctx.send("PLease join a voice channel first")
-        sid = str(ctx.guild.id)
-        vcid = str(ctx.message.author.voice.channel.id)
-                
+        # try except block to handle if user not in vc        
+         
         await ctx.send(text_string.format(voicechannel = vc) + link_string.format(serverid = sid, voicechannelid = vcid))
