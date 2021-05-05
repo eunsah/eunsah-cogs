5a748b20b19421ae2b4d9007454cab4eb7428fd6
eunsah <xchedeiklo@gmail.com>
2021-05-05 17:39:30 +0800

    small changes

--- exp/exp.py
+++ exp/exp.py
@@ -3,6 +3,7 @@ import asyncio
 import datetime
 import logging
 import discord
+import json
 from redbot.core import commands, checks, Config
 log=logging.getLogger("red.eunsahcogs.exp")
 
@@ -22,6 +23,21 @@ class Exp(commands.Cog):
         self.config.register_user(**default_user)
 
 
-    @commands.command()
-    async def return_some_data(self, ctx):
-        await ctx.send(await self.config.foo())
+    @checks.is_owner()
+    @exp.command()
+    async def info(self,ctx):
+        name = await self.config.user(ctx.author).name
+        level = await self.config.user(ctx.author).level
+        exp = await self.config.user(ctx.author).exp
+        previous_date = await self.config.user(ctx.author).previous_date
+
+        e = discord.Embed(
+            title = "Character Info",
+            description = "Last update: "+previous_date,
+            color = ctx.author.color
+        )
+        e.add_field(name="Name", value=name, inline=False)
+        e.add_field(name="Exp", value=exp, inline=True)
+        e.add_field(name="Level", value=level, inline=True)
+
+        ctx.send(e)
