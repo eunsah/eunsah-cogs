4c2e98d6ff3bc11a09396cc71bd7b180d753860d
eunsah <xchedeiklo@gmail.com>
2021-05-06 14:46:45 +0800

    small changes

--- exp/exp.py
+++ exp/exp.py
@@ -28,41 +28,11 @@ class Exp(commands.Cog):
         }
         self.config.register_user(**default_user)
 
-    async def embedout(self, ctx):
-        name = await self.config.user(ctx.author).name()
-        level = await self.config.user(ctx.author).level()
-        exp = await self.config.user(ctx.author).exp()
-        previous_date = await self.config.user(ctx.author).previous_date()
-        daily_velocity = await self.config.user(ctx.author).daily_velocity()
-
-        e = discord.Embed(
-            title = 'Character Info',
-            description = 'Last update: ' + datetime.datetime.fromtimestamp(previous_date).strftime('%Y/%m/%d'),
-            color = ctx.author.color
-        )
-        e.add_field(name="Name", value=name, inline=True)
-        e.add_field(name="Level", value=level, inline=True)
-        e.add_field(name="Exp", value=exp, inline=False)
-        e.add_field(name="Daily total Average", value=str(round(daily_velocity))+' exp per day', inline=False)
-
-        return e
-
-    @checks.is_owner()
-    @commands.command()
-    async def msinfo(self,ctx):
-        await ctx.send(embed=await self.embedout(ctx))
-
-    @checks.is_owner()
-    @commands.command()
-    async def exp(self, ctx, *argv):
+    async def levelexp_verification(self, ctx, *kwargs):
         '''
-            [p]exp {level} {percentage || raw exp}
-            Update exp
+        Verify level, exp and sets level, exp, raw
+        parameters : ctx, level, exp
         '''
-        if len(argv) != 2:
-            # argv check
-            await ctx.send(f'Not enough arguments')
-            return
         level = int(argv[0])
         exp = argv[1]
         if level < 0 or level > MAX_LEVEL:
@@ -87,20 +57,64 @@ class Exp(commands.Cog):
             if int(key) == level:
                 break
         raw += exp
-        # level exp raw
 
-        await self.config.user(ctx.author).level.set(level)
-        await self.config.user(ctx.author).exp.set(exp)
+        await self.config.user(ctx.author).level.set(int(level))
+        await self.config.user(ctx.author).exp.set(int(exp))
+        await self.config.user(ctx.author).raw.set(int(raw))
+
+    async def embedout(self, ctx) -> discord.Embed:
+        name = await self.config.user(ctx.author).name()
+        level = await self.config.user(ctx.author).level()
+        exp = await self.config.user(ctx.author).exp()
+        previous_date = await self.config.user(ctx.author).previous_date()
+        daily_velocity = await self.config.user(ctx.author).daily_velocity()
+
+        e = discord.Embed(
+            title = 'Character Info',
+            description = 'Last update: ' + datetime.datetime.fromtimestamp(previous_date).strftime('%Y/%m/%d'),
+            color = ctx.author.color
+        )
+        e.add_field(name="Name", value=name, inline=True)
+        e.add_field(name="Level", value=level, inline=True)
+        e.add_field(name="Exp", value=exp, inline=False)
+        e.add_field(name="Average Daily Exp (Total)", value=str(round(daily_velocity))+' exp per day', inline=False)
+
+        return e
+
+    @checks.is_owner()
+    @commands.command()
+    async def msinfo(self,ctx):
+        await ctx.send(embed=await self.embedout(ctx))
+
+    @checks.is_owner()
+    @commands.command()
+    async def exp(self, ctx, *argv):
+        '''
+            [p]exp {level} {percentage || raw exp}
+            Update exp
+        '''
+        if len(argv) != 2:
+            # argv check
+            await ctx.send(f'Not enough arguments')
+            return
+
+        level = argv[0]
+        raw = await self.config.user(ctx.author).raw()
+        previous_date_datetime = datetime.datetime.fromtimestamp(await self.config.user(ctx.author).previous_date())
+
+        await self.levelexp_verification(ctx, level=argv[0], exp=argv[1])
+
         await self.config.user(ctx.author).previous_date.set(datetime.datetime.timestamp(datetime.datetime.now()))
 
+        raw_diff = await self.config.user(ctx.author).raw() - raw
+        date_diff_timedelta = datetime.datetime.fromtimestamp(await self.config.user(ctx.author).previous_date()) - previous_date
+
+        raw_diff_percentage = round((raw_diff / self.levelchart[str(level)])*100, 2)
+        avg_exp = round(raw_diff/(date_diff_timedelta.total_seconds()/86400), 2) # 86400 is the total seconds in a day
+
         e = await self.embedout()
-
-        e.add_field(name="Exp gain", value=exp, inline=True)
-        e.add_field(name="Daily since Last Update", value=exp, inline=True)
-
-
-
-
+        e.add_field(name="Average Daily Exp (Update)", value=avg_exp, inline=True)
+        e.add_field(name="Total Exp Growth", value=str(raw_diff) + ' (' + str(raw_diff_percentage) + '%)', inline=True)
 
     @checks.is_owner()
     @commands.group()
@@ -111,12 +125,10 @@ class Exp(commands.Cog):
     @expset.command()
     async def init(self, ctx, name='角色', level=0, exp=0, date=datetime.datetime.now().strftime('%Y/%m/%d')):
 
-        previous_date = datetime.datetime.strptime(date, '%Y/%m/%d')
+        await self.levelexp_verification(ctx, level=level, exp=exp)
         await self.config.user(ctx.author).name.set(name)
-        await self.config.user(ctx.author).level.set(level)
-        await self.config.user(ctx.author).exp.set(exp)
+        previous_date = datetime.datetime.strptime(date, '%Y/%m/%d')
         await self.config.user(ctx.author).previous_date.set(datetime.datetime.timestamp(previous_date))
-        # await self.config.user(ctx.author).daily_velocity.set(default_user['daily_velocity'])
 
-        await ctx.send(f'user value has been reseted.')
+        await ctx.send(f'user value has been initialized.')
 
