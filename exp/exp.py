b78d0f42699f92a69a1bb5f97291d761df97fd95
eunsah <xchedeiklo@gmail.com>
2021-05-07 14:16:28 +0800

    small changes

--- exp/exp.py
+++ exp/exp.py
@@ -23,7 +23,7 @@ class Exp(commands.Cog):
         self.bot = bot
         with open(os.path.join(dir_path, folder, level_json)) as j:
             self.levelchart = json.load(j)
-        self.config = Config.get_conf(self, identifier=int(str(auid)+'001')),  force_registration=True)
+        self.config = Config.get_conf(self, identifier=int(str(auid)+'001'),  force_registration=True)
         default_user = {
             'name':'角色',
             'level' : 1,
