5b916bfb1f17ac6bdba6e66160dccfa7039a2f7e
eunsah <xchedeiklo@gmail.com>
2021-05-10 18:01:28 +0800

    update

--- maplexp/maplexp.py
+++ maplexp/maplexp.py
@@ -173,7 +173,7 @@ class Maplexp(commands.Cog):
         e.add_field(name='名稱', value=data_d['name'], inline=True)
         e.add_field(name='等級', value=level, inline=True)
         e.add_field(name='經驗值', value=f'{exp:,} ({round((exp/req_exp)*100, 2):.2f}%)', inline=False)
-        e.add_field(name='經驗成長日平均', value=f'{data_d['avg_exp']:,}', inline=False)
+        e.add_field(name='經驗成長日平均', value=f'{round(0.0):,}', inline=False)
         e.set_footer(text='更新日期: ' + datetime.datetime.fromtimestamp(data_d['previous_date']).strftime('%Y/%m/%d'))
 
         return e
