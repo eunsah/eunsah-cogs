cdaaf4e7026d7fe92f04dacf0cee584f0840af4e
eunsah <xchedeiklo@gmail.com>
2021-05-11 18:06:53 +0800

    update

--- maplexp/maplexp.py
+++ maplexp/maplexp.py
@@ -509,7 +509,7 @@ class Maplexp(commands.Cog):
             return
         await verify.delete()
 
-        await self.config.clear()
+        await self.config.clear_all()
         await ctx.tick()
         await self._remove_after_seconds(ctx.message, MESSAGE_REMOVE_DELAY)
 
