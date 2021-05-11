50cc7d4099d048eef69389eb69c47db398bc3dd7
eunsah <xchedeiklo@gmail.com>
2021-05-11 17:58:16 +0800

    update

--- maplexp/maplexp.py
+++ maplexp/maplexp.py
@@ -34,9 +34,8 @@ class Maplexp(commands.Cog):
             'date' : self.base_time
         }
         default_user = {
-            'ptr_dvfdvdf' : '',
-            'usr_dfdfvdfvdf': {
-            }
+            'ptr_d' : '',
+            'usr_d': ''
         }
         self.config.register_user(**default_user)
 
